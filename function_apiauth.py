b7ee9ebe814302c151330e89ebc588a09dd15e81
takana303 <msdf303@live.jp>
2018-11-25 23:28:44 +0900

    Update function_apiauth.py

--- function_apiauth.py
+++ function_apiauth.py
@@ -1,20 +1,20 @@
 # _*_ coding: utf-8 _*_
 
-import tweepy
-
-### 認証 ###
-
-def apiauth():
-	twitter_conf = {
-		'consumer' : {
-			'key'   : "",
-			'secret' : ""
-		},
-		'access'   : {
-			'key'   : "",
-			'secret' : ""
-		}
+twitter_conf = {
+	'consumer' : {
+		'key'   : "",
+		'secret' : ""
+	},
+	'access'   : {
+		'key'   : "",
+		'secret' : ""
 	}
+}
+
+### tweepy ###
+
+def tweepyauth():
+	import tweepy
 	auth = tweepy.OAuthHandler(
 		twitter_conf['consumer']['key'],
 		twitter_conf['consumer']['secret'])
@@ -23,3 +23,15 @@ def apiauth():
 		twitter_conf['access']['secret'])
 	tweepy_auth = tweepy.API(auth)
 	return(tweepy_auth)
+
+### OAuth ###
+
+def oaauth():
+	from requests_oauthlib import OAuth1Session
+	twitter = OAuth1Session(
+		twitter_conf['consumer']['key'],
+		twitter_conf['consumer']['secret'],
+		twitter_conf['access']['key'],
+		twitter_conf['access']['secret'],
+	)
+	return(twitter)
